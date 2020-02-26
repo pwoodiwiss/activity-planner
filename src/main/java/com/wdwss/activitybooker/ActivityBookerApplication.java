b40e577e2bff2428bc10dcdbfa8ee7025246977d
Peter Woodiwiss <2631798+pwoodiwiss@users.noreply.github.com>
2020-02-26 22:49:03 +0000

    Initial Application

---
+++ src/main/java/com/wdwss/activitybooker/ActivityBookerApplication.java
@@ -0,0 +1,13 @@
+package com.wdwss.activitybooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityBookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivityBookerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wdwss/activitybooker/ActivityBookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wdwss.activitybooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityBookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
